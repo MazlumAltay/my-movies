5abab52fbdc0a8b79a8c47f3870b311720b2bd93
Mazlum Altay <79927062+MazlumAltay@users.noreply.github.com>
2022-07-31 14:02:11 +0300

    MovieList (Map)

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,25 +1,38 @@
 import React from "react";
 
 class MovieList extends React.Component {
-    render(){
-        return(
-            <div className="row">
-                <div className="col-lg-4">
-                    <div className="card mb-4 shadow-sm">
-                        <img src={`https://www.themoviedb.org/t/p/original/gTlZAMmuMjN2Ia0M4pxU2C0ZWxq.jpg`} className="card-img-top" alt="sample movie" />
-                        <div className="card-body">
-                            <h5 className="card-title">Sample movie</h5>
-                            <p className="card-text">Sample movie Description</p>
-                            <div className="d-flex justify-content-between align-items-center">
-                                <button type="button" className="btn btn-md btn-outline-danger">Delete</button>
-                                <h2><span class="badge bg-secondary">9.0</span></h2>
-                            </div>
-                        </div>
-                    </div>
+  render() {
+    return (
+      <div className="row">
+        {this.props.movies.map((movie) => (
+          <div className="col-lg-4" key={movie.id}>
+            <div className="card mb-4 shadow-sm">
+              <img
+                src={movie.imageURL}
+                className="card-img-top"
+                alt="sample movie"
+              />
+              <div className="card-body">
+                <h5 className="card-title">{movie.name}</h5>
+                <p className="card-text">{movie.overview}</p>
+                <div className="d-flex justify-content-between align-items-center">
+                  <button
+                    type="button"
+                    className="btn btn-md btn-outline-danger"
+                  >
+                    Delete
+                  </button>
+                  <h2>
+                    <span class="badge bg-secondary">{movie.rating}</span>
+                  </h2>
                 </div>
-        </div>
-        )
-    }
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default MovieList;
