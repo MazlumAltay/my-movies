bb3b42ec51b509eaf874a8683bcf0439e2393e0f
Mazlum Altay <79927062+MazlumAltay@users.noreply.github.com>
2022-08-01 16:20:47 +0300

    state update

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,18 +1,31 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-    render(){
-        return(
-            <form>
-                <div className="form-row mb-5">
-                    <div className="col-12">
-                        <input type="text" className="form-control" placeholder="Search a movie" />
 
-                    </div>
-                </div>
-            </form>
-        )
+    state = {
+        serachQuery: ""
     }
+
+    handleFormSubmit = (event) => {
+        event.preventDefault();
+    }
+  render() {
+    return (
+      <form onSubmit={this.handleFormSubmit}>
+        <div className="form-row mb-5">
+          <div className="col-12">
+            <input
+            onChange={(event) => this.setState({serachQuery: event.target.value})}
+              type="text"
+              className="form-control"
+              placeholder="Search a movie"
+              value={this.state.serachQuery}
+            />
+          </div>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default SearchBar;
