a55401c74ee3f612a6637d058a02c74ea087e814
Mazlum Altay <79927062+MazlumAltay@users.noreply.github.com>
2022-08-03 16:42:06 +0300

    Fetch and Axios.!

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import MovieList from "./MovieList";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
@@ -9,21 +10,49 @@ class App extends React.Component {
     searchQuery: "",
   };
 
-  async componentDidMount(){
-    const baseURL = "http://localhost:3002/movies";
-    const response = await fetch(baseURL);
-    console.log(response)
-    const data = await response.json();
-    console.log(data)
-    this.setState({movies: data})
-  }
+  /*
+   async componentDidMount(){
+     const baseURL = "http://localhost:3002/movies";
+     const response = await fetch(baseURL);
+     console.log(response)
+     const data = await response.json();
+     console.log(data)
+     this.setState({movies: data})
+   }
+   */
 
+  async componentDidMount() {
+    const response = await axios.get("http://localhost:3002/movies");
+    //console.log(response);
+    this.setState({ movies: response.data });
+  }
+  /*
   deleteMovie = (movie) => {
+     const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
+     this.setState((state) => ({
+       movies: newMovieList,
+    }));
+  */
+
+  /*
+  // FETCH APİ
+  deleteMovie = async (movie) => {
+    const baseURL = `http://localhost:3002/movies/${movie.id}`;
+    await fetch(baseURL, { method: "DELETE" });
+
     const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
 
-    // this.setState({
-    //   movies: newMovieList,
-    // });
+    this.setState((state) => ({
+      movies: newMovieList,
+    }));
+  };
+  */
+
+  // AXİOS APİ
+  deleteMovie = async (movie) => {
+    axios.delete(`http://localhost:3002/movies/${movie.id}`)
+
+    const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
 
     this.setState((state) => ({
       movies: newMovieList,
@@ -32,16 +61,17 @@ class App extends React.Component {
 
   searchMovie = (event) => {
     //console.log(event.target.value);
-    this.setState({searchQuery: event.target.value})
+    this.setState({ searchQuery: event.target.value });
   };
 
   render() {
-
-    let filteredMovies =  this.state.movies.filter(
-      (movie) =>{
-        return movie.name.toLowerCase().indexOf(this.state.searchQuery.toLowerCase()) !== -1
-      }
-    )
+    let filteredMovies = this.state.movies.filter((movie) => {
+      return (
+        movie.name
+          .toLowerCase()
+          .indexOf(this.state.searchQuery.toLowerCase()) !== -1
+      );
+    });
     return (
       <div className="container">
         <div className="row">
@@ -49,10 +79,7 @@ class App extends React.Component {
             <SearchBar searchMovieProp={this.searchMovie} />
           </div>
         </div>
-        <MovieList
-          movies={filteredMovies}
-          deleteMovieProp={this.deleteMovie}
-        />
+        <MovieList movies={filteredMovies} deleteMovieProp={this.deleteMovie} />
       </div>
     );
   }
